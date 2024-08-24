7b938146fcb6ec71ff51bf3345f7a393659f7404
aftabshahmadar <aftabshahmadar4745@gmail.com>
2024-08-24 17:49:55 +0530

    all data is uploaded

---
+++ module 3/hello word app/hello-word-app/src/index.js
@@ -0,0 +1,13 @@
+/* import React from 'react';
+import ReactDOM from 'react-DOM/client'
+const root=ReactDOM.createRoot(document.getElementById("demo"));
+root.render (
+    <h1>hello word</h1>
+)*/
+
+import React from "react";
+import ReactDOM from 'react-dom/client'
+const root=ReactDOM.createRoot(document.getElementById("demo"));
+root.render(
+<h1 align='center' style={{color:"grey",fontSize:"200px"}}>Hello world</h1>
+)
